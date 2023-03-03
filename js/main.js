542be236c8619990d2f6a2125aeb33989586c18c
gevansscc <76914070+gevansscc@users.noreply.github.com>
2023-03-03 12:15:05 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,6 +39,19 @@ $(function() {
 			$('header').removeClass('header-animate-fade-out').addClass('header-animate-fade-in');
 		}
   });
+
+  $( "#external" ).click(function() {
+
+    $('.off-screen-menu').toggleClass('animate');
+    $('.svg').toggleClass('filter-green');
+		if($('header').attr('class') == null ){
+			$('header').addClass('header-animate-fade-in');
+		} else if($('header').attr('class') == 'header-animate-fade-in'){
+			$('header').removeClass('header-animate-fade-in').addClass('header-animate-fade-out');
+		} else if(($('header').attr('class') == 'header-animate-fade-out')){
+			$('header').removeClass('header-animate-fade-out').addClass('header-animate-fade-in');
+		}
+  });
 	
 	
 });
